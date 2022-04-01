d122547c9a40a0044ebefb9a973e39d3b96d742b
lshini <99379483+lshini@users.noreply.github.com>
2022-04-02 02:52:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/predict", methods=['GET', 'POST'])
+@app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
         file = request.files['image']
@@ -38,7 +38,7 @@ def predict():
         edged = cv2.Canny(blurred, 30, 150)
         cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
-        cnts = sorted(cnts, key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2.boundingRect(ctr)[1] * image.shape[2] )
+        cnts = sorted(cnts, key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2.boundingRect(ctr)[1] * src.shape[2] )
         chars = []
         for c in cnts:
             # compute the bounding box of the contour
