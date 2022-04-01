de4187cb0186652221fdfd5ab5bd70b5981f801a
lshini <99379483+lshini@users.noreply.github.com>
2022-04-02 03:50:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,79 +25,78 @@ def home():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        file = request.files['image']
-        filename = file.filename
-        file_path = os.path.join('statics/user uploaded/', filename)
-        file.save(file_path)
-        test_image = tf.keras.preprocessing.image.load_img(file_path)
-        src = cv2.imread(file_path)
-        print(src)
-        gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-        edged = cv2.Canny(blurred, 30, 150)
-        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        cnts = sorted(cnts, key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2.boundingRect(ctr)[1] * src.shape[2] )
-        chars = []
-        for c in cnts:
-            # compute the bounding box of the contour
-            (x, y, w, h) = cv2.boundingRect(c)
-            # filter out bounding boxes, ensuring they are neither too small
-            # nor too large
-            if (w >= 5 and w <= 150) and (h >= 15 and h <= 120):
-                # extract the character and threshold it to make the character
-                # appear as *white* (foreground) on a *black* background, then
-                # grab the width and height of the thresholded image
-                roi = gray[y:y + h, x:x + w]
-                thresh = cv2.threshold(roi, 0, 255,
-                                       cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-                (tH, tW) = thresh.shape
-                # if the width is greater than the height, resize along the
-                # width dimension
-                if tW > tH:
-                    thresh = imutils.resize(thresh, width=32)
-                # otherwise, resize along the height
-                else:
-                    thresh = imutils.resize(thresh, height=32)
-                # re-grab the image dimensions (now that its been resized)
-                # and then determine how much we need to pad the width and
-                # height such that our image will be 32x32
-                (tH, tW) = thresh.shape
-                dX = int(max(0, 32 - tW) / 2.0)
-                dY = int(max(0, 32 - tH) / 2.0)
-                # pad the image and force 32x32 dimensions
-                padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
-                                            left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
-                                            value=(0, 0, 0))
-                padded = cv2.resize(padded, (32, 32))
-                # prepare the padded image for classification via our
-                # handwriting OCR model
-                padded = padded.astype("float32") / 255.0
-                padded = np.expand_dims(padded, axis=-1)
-                # update our list of characters that will be OCR'd
-                chars.append((padded, (x, y, w, h)))
-        boxes = [b[1] for b in chars]
-        chars = np.array([c[0] for c in chars], dtype="float32")
-        # OCR the characters using our handwriting recognition model
-        preds = model.predict(chars)
-        # define the list of label names
-        labelNames = "0123456789"
-        labelNames += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        labelNames += "abdefghnqrt"
-        labelNames = [l for l in labelNames]
+    file = request.files['image']
+    filename = file.filename
+    file_path = os.path.join('statics/user uploaded/', filename)
+    file.save(file_path)
+    test_image = tf.keras.preprocessing.image.load_img(file_path)
+    src = cv2.imread(file_path)
+    print(src)
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(blurred, 30, 150)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2.boundingRect(ctr)[1] * src.shape[2] )
+    chars = []
+    for c in cnts:
+        # compute the bounding box of the contour
+        (x, y, w, h) = cv2.boundingRect(c)
+        # filter out bounding boxes, ensuring they are neither too small
+        # nor too large
+        if (w >= 5 and w <= 150) and (h >= 15 and h <= 120):
+            # extract the character and threshold it to make the character
+            # appear as *white* (foreground) on a *black* background, then
+            # grab the width and height of the thresholded image
+            roi = gray[y:y + h, x:x + w]
+            thresh = cv2.threshold(roi, 0, 255,
+                                   cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            (tH, tW) = thresh.shape
+            # if the width is greater than the height, resize along the
+            # width dimension
+            if tW > tH:
+                thresh = imutils.resize(thresh, width=32)
+            # otherwise, resize along the height
+            else:
+                thresh = imutils.resize(thresh, height=32)
+            # re-grab the image dimensions (now that its been resized)
+            # and then determine how much we need to pad the width and
+            # height such that our image will be 32x32
+            (tH, tW) = thresh.shape
+            dX = int(max(0, 32 - tW) / 2.0)
+            dY = int(max(0, 32 - tH) / 2.0)
+            # pad the image and force 32x32 dimensions
+            padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
+                                        left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
+                                        value=(0, 0, 0))
+            padded = cv2.resize(padded, (32, 32))
+            # prepare the padded image for classification via our
+            # handwriting OCR model
+            padded = padded.astype("float32") / 255.0
+            padded = np.expand_dims(padded, axis=-1)
+            # update our list of characters that will be OCR'd
+            chars.append((padded, (x, y, w, h)))
+    boxes = [b[1] for b in chars]
+    chars = np.array([c[0] for c in chars], dtype="float32")
+    # OCR the characters using our handwriting recognition model
+    preds = model.predict(chars)
+    # define the list of label names
+    labelNames = "0123456789"
+    labelNames += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    labelNames += "abdefghnqrt"
+    labelNames = [l for l in labelNames]
 
-        output = ""
-        for (pred, (x, y, w, h)) in zip(preds, boxes):
-            i = np.argmax(pred)
-            prob = pred[i]
-            label = labelNames[i]
-            output += label
+    output = ""
+    for (pred, (x, y, w, h)) in zip(preds, boxes):
+        i = np.argmax(pred)
+        prob = pred[i]
+        label = labelNames[i]
+        output += label
 
-        print("output",output)
+    print("output",output)
 
-        return render_template('index.html', pred_output=output, user_image=file_path)
+    return render_template('index.html', pred_output=output, user_image=file_path)
 
 
 if __name__ == "__main__":
-    app.run(threaded=False)
+    app.run(debug=True)
